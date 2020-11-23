1c63bbd2aaefd09236a1ef723a3e2d02cedf71cd
zekiiio <zekio@hotmail.com>
2020-11-23 17:27:33 +0800

    增加分页

--- src/components/blogs/blogs.vue
+++ src/components/blogs/blogs.vue
@@ -29,8 +29,7 @@
       </el-table-column>
       <el-table-column
         prop="title"
-        label="Title"
-        width="300">
+        label="Title">
       </el-table-column>
       <el-table-column
         prop="theme"
@@ -38,16 +37,54 @@
         width="100">
       </el-table-column>
       <el-table-column
-        prop="create_time"
         label="Created"
-        width="180">
+        width="150">
+        <!--如果单元格内显示的内容不是字符串(文本)，
+        需要给被显示的内容外层包裹一个template-->
+        <!--slot-scope会自动寻找最近的外层标签绑定的数据，
+        即使赋值abc也会寻找到bloglist-->
+        <template slot-scope="bloglist">
+          <!--template内部要使用数据，设置slot-scope属性
+          属性值是要用数据的数据源,即el-table绑定的bloglist
+          但bloglist是一个数组，bloglist.row指的是数组中每个对象-->
+          {{bloglist.row.create_time | fmtdate}}
+          <!--el-table-column中prop的值无法传给内部的template，
+          因此使用上述方法，可删除prop-->
+        </template>
       </el-table-column>
       <el-table-column
-        prop="is_active"
         label="Active"
         width="100">
+        <template slot-scope="scope">
+          <el-switch
+            v-model="scope.row.is_active"
+            active-color="lightblue"
+            inactive-color="gray">
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column width="200">
+        <template>
+          <el-row>
+            <el-button type="primary" icon="el-icon-edit" size="mini" circle></el-button>
+            <el-button type="success" icon="el-icon-check" size="mini" circle></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini" circle></el-button>
+          </el-row>
+        </template>
       </el-table-column>
     </el-table>
+
+    <div class="block">
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="pagenum"
+        :page-sizes="[2, 4, 6, 8]"
+        :page-size="2"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
+    </div>
 
   </el-card>
 </template>
@@ -60,7 +97,7 @@
           query: '',
           pagenum: 1,
           pagesize: 2,
-          total: 1,
+          total: 3,
           bloglist: []
         }
       },
@@ -71,16 +108,34 @@
         async getBlogList(){
           //需要授权的请求，必须设置请求头
           /*const AUTH_TOKEN = localStorage.getItem('token')
-          this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN*/
+          this.$http.defau,lts.headers.common['Authorization'] = AUTH_TOKEN*/
+          var res
+          //搜索+分页
+          if(this.query===''){
+            res = await this.$http.get(`blogs?pagenum=${this.pagenum}&pagesize=${this.pagesize}`)
+          }else{
+            res = await this.$http.get(
+              `blogs?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`)
+          }
+          //获取所有
+          /*const res = await this.$http.get('blogs')*/
 
-          //query为搜索中的数据
-          /*const res = await this.$http.get(
-            'blogs?query=' + this.query +
-            '&pagenum=' + this.pagenum +
-            '&pagesize=' + this.pagesize)*/
-          const res = await this.$http.get('blogs')
-          const {data} = res.data
-          this.bloglist = data
+          const {
+            code,
+            data: { total, list }
+          } = res.data
+          if(code === 20000){
+            this.bloglist = list
+            this.total = total
+          }
+        },
+        handleSizeChange(val) {
+          this.pagesize = val
+          this.getBlogList()
+        },
+        handleCurrentChange(val) {
+          this.pagenum = val
+          this.getBlogList()
         }
       }
     }
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import router from './router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import locale from "element-ui/lib/locale/lang/en";
-Vue.use(ElementUI), {locale};
+Vue.use(ElementUI, {locale});
 //导入自定义的style
 import '@/assets/css/reset.css'
 //导入自动以插件
@@ -14,6 +14,12 @@ import MyHttpServer from "@/plugins/http.js";
 Vue.use(MyHttpServer);
 
 Vue.config.productionTip = false;
+
+import moment from "moment";
+//fmtdate全局过滤器 - 处理日期
+Vue.filter('fmtdate',(v)=>{
+  return moment(v).format('YYYY-MM-DD')
+})
 
 /* eslint-disable no-new */
 new Vue({
